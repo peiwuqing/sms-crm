fd348eb1dc155d8b8bfb4241caa49df32edc18ab
peiwuqing <peiwq@petbanny.com>
2022-08-26 15:54:27 +0800

    修改配置文件

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,27 +6,12 @@ let baseUrl = ''
 let phoneBarConf = {}
 switch (params) {
   case '--env=test':
-    // phoneBarConf = {
-    //   socketUrl:'"ws://8.142.69.19:8081/agents"',
-    //   domain:'"founder"',
-    //   key:'"93ef9a4bc4ed4379f8e7d952899f9c6b"'
-    // };
     baseUrl = '"http://127.0.0.1:17098/"';
     break
   case '--env=prod':
-    // phoneBarConf = {
-    //   socketUrl:'"ws://8.142.69.19:8081/agents"',
-    //   domain:'"founder"',
-    //   key:'"93ef9a4bc4ed4379f8e7d952899f9c6b"'
-    // };
     baseUrl = '"http://127.0.0.1:17098/"';
     break
   default:
-    // phoneBarConf = {
-    //   socketUrl:'"ws://8.142.69.19:8081/agents"',
-    //   domain:'"founder"',
-    //   key:'"93ef9a4bc4ed4379f8e7d952899f9c6b"'
-    // };
     baseUrl = '"http://127.0.0.1:17098/"';
 }
 module.exports = merge(prodEnv, {
--- config/prod.env.js
+++ config/prod.env.js
@@ -5,27 +5,12 @@ let phoneBarConf = {}
 switch (params) {
   case '--env=test':
     baseUrl = '"http://oss-server.xb-sms.com/"';
-    // phoneBarConf = {
-    //   socketUrl:'"ws://8.142.69.19:8081/agents"',
-    //   domain:'"founder"',
-    //   key:'"93ef9a4bc4ed4379f8e7d952899f9c6b"'
-    // };
     break
   case '--env=prod':
     baseUrl = '"http://oss-server.xb-sms.com/"';
-    // phoneBarConf = {
-    //   socketUrl:'"ws://8.142.69.19:8081/agents"',
-    //   domain:'"founder"',
-    //   key:'"93ef9a4bc4ed4379f8e7d952899f9c6b"'
-    // };
     break
   default:
     baseUrl = '"http://oss-server.xb-sms.com/"';
-    // phoneBarConf = {
-    //   socketUrl:'"ws://8.142.69.19:8081/agents"',
-    //   domain:'"founder"',
-    //   key:'"93ef9a4bc4ed4379f8e7d952899f9c6b"'
-    // };
 }
 module.exports = {
   NODE_ENV: '"production"',
