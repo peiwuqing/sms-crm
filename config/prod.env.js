2fe9c20ec7cc1a7c7f3f842c12bbe52279d30bf9
peiwuqing <peiwq@petbanny.com>
2023-04-11 21:24:32 +0800

    d

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,13 +4,13 @@ let baseUrl = ''
 let phoneBarConf = {}
 switch (params) {
   case '--env=test':
-    baseUrl = '"http://oss-server.xb-sms.com/"';
+    baseUrl = '"https://oss-server.xb-sms.com/"';
     break
   case '--env=prod':
-    baseUrl = '"http://oss-server.xb-sms.com/"';
+    baseUrl = '"https://oss-server.xb-sms.com/"';
     break
   default:
-    baseUrl = '"http://oss-server.xb-sms.com/"';
+    baseUrl = '"httpsss://oss-server.xb-sms.com/"';
 }
 module.exports = {
   NODE_ENV: '"production"',
